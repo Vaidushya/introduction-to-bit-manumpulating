962d72ae777a17b2fe02d040439705c7bff82da5
Vaidushya <anugya2014@gmail.com>
2026-05-03 16:44:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Program to showcase working of different types of bitwise operators
+
+num1 = 10
+num2 = 4
+
+# using NOT operators
+print(f"~num1 = {~num1}")
+print(f"\n ~num2 = {~num2}")
+
+# using AND operators
+print(f"\n num1 & num2 = {num1 & num2}")
+
+# using OR operators
+print(f"\n num1 | num2 = {num1 | num2}")
+
+# using XOR operators
+print(f"\n num1 ^ num2 = {num1 ^ num2}")
+
+# using left shift operators on num1
+print(f"\n num1 << 2 = {num1 << 1}")
+
+# using left shift operators on num2
+print(f"\n num2 << 1 = {num2 << 1}")
+
+# using right shift operators
+print(f"\n num1 >> 1 = {num1 >> 1}")
+
+# using right shift on num2
+print(f"\n num2 >> 1 = {num2 >> 1}")
---
+++ main2.py
@@ -0,0 +1,29 @@
+# programme to showcase working of different type of bitwise operators
+
+num1 = 12
+num2 = 8
+
+# un=sing NOT operators
+print(f"~num1 = {~num1}")
+print(f"\n ~num2 = {~num2}")
+
+# using AND operators
+print(f"\n num1 & num2 = {num1 & num2}")
+
+# using OR operators
+print(f"\n num1 | num2 = {num1 | num2}")
+
+# using XOR operators
+print(f"\n num1 ^ num2 = {num1 ^ num2}")
+
+# using left shift on nunm1
+print(f"\n num1 << 1 = {num1 << 1}")
+
+# using left shift on num2
+print(f"\n num2 << 1 = {num2 << 1}")
+
+# using right shift on num1
+print(f"\n num1 >> 1 = {num1 >> 1}")
+
+# using right shift on num2
+print(f"\n num2 >> 1 = {num2 >> 1}")
