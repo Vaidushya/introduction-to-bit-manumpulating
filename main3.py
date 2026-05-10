ef84809e9ab476526d585c3490e498c040e9b644
Vaidushya <anugya2014@gmail.com>
2026-05-10 14:04:30 +0530

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,13 @@
+def isEvenOdd( n) :
+    # XOR with 1 equals n+1
+    if (n ^ 1 == n + 1) :
+        return True
+    else :
+        return False
+ 
+number = int(input("Enter your number : "))
+ 
+if isEvenOdd(number):
+    print(number," is Even")
+else:
+    print(number," is Odd")
